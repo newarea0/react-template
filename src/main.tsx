d95ba9e2a548f3fc86bbee7288004e76a1b1a85d
stormzhangbx <13128814051@163.com>
2024-10-24 15:12:20 +0800

    feat: 集成 Redux

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,14 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { Provider } from 'react-redux'
 import { RouterProvider } from 'react-router-dom'
 import router from './router/index.tsx'
+import { store } from './store'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <RouterProvider router={router} />
+    <Provider store={store}>
+      <RouterProvider router={router} />
+    </Provider>
   </StrictMode>,
 )
--- src/views/home/index.tsx
+++ src/views/home/index.tsx
@@ -1,3 +1,23 @@
-export default function Index() {
-  return <div>Home</div>
+import type { RootState } from '../../store'
+import React from 'react'
+import { useDispatch, useSelector } from 'react-redux'
+import { decrement, increment } from '../../store/counterSlice'
+
+const Home: React.FC = () => {
+  const count = useSelector((state: RootState) => state.counter.value)
+  const dispatch = useDispatch()
+
+  return (
+    <div>
+      <h1>首页</h1>
+      <p>
+        计数:
+        {count}
+      </p>
+      <button type="button" onClick={() => dispatch(increment())}>增加</button>
+      <button type="button" onClick={() => dispatch(decrement())}>减少</button>
+    </div>
+  )
 }
+
+export default Home
